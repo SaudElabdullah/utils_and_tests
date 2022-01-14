78bba41e4a082e59e8a1c14d5e29b7d614d8b517
Saud <Soa142812222@gmail.com>
2022-01-14 15:32:43 +0300

    Added platform_detection.dart.

---
+++ lib/utils/platform_detection.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+
+class PlatformDetection {
+  static bool isDesktop = defaultTargetPlatform == TargetPlatform.windows ||
+      defaultTargetPlatform == TargetPlatform.linux ||
+      defaultTargetPlatform == TargetPlatform.macOS;
+  static bool isMobile = defaultTargetPlatform == TargetPlatform.android ||
+      defaultTargetPlatform == TargetPlatform.iOS;
+  static bool isWeb = kIsWeb;
+}
